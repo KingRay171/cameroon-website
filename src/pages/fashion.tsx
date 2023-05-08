d7c75a84aa5eac8be14dd0586af02ad32ceba9b4
KingRay171 <raycikome@gmail.com>
2023-05-08 11:44:20 -0400

    finished fashion page and citations

--- src/pages/fashion.tsx
+++ src/pages/fashion.tsx
@@ -1,6 +1,74 @@
+import { Inter } from "next/font/google";
+import Image from "next/image";
+import { Tooltip } from "@nextui-org/react";
+const inter = Inter({ subsets: ["latin"] });
 
-export default function Fashion(){
-    return (
-        <></>
-    )
+export default function Fashion() {
+  const styles = [
+    {
+      src: "/toghu.jpg",
+      alt: "Toghu",
+      tooltip: "Le Toghu",
+      key: "fashion1",
+    },
+    {
+      src: "/ndop.avif",
+      alt: "Ndop",
+      tooltip: "Le Ndop",
+      key: "fashion2",
+    },
+    {
+      src: "/kaba2.jpg",
+      alt: "Kaba",
+      tooltip: "Le Kaba-Ngondo",
+      key: "fashion3",
+    },
+    {
+      src: "/grand-nord.png",
+      alt: "Grand-Nord",
+      tooltip: "Le Grand-Nord",
+      key: "fashion4",
+    },
+  ];
+  return (
+    <main
+      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+    >
+      <div
+        className="relative flex place-items-center text-6xl
+            before:absolute before:h-[300px] before:w-[480px]  before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-['']
+            after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-['']
+            before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]"
+      >
+        <h1>
+          La Mode du <br></br>Cameroun
+        </h1>
+      </div>
+
+      <div className="flex flex-wrap w-full -m-4 mt-32 ">
+        {styles.map((style) => (
+          <div className="sm:w-1/2 w-100 p-4" key={style.key}>
+            <div className="flex relative h-full ">
+              <div className="px-4 py-4 relative z-10 border-4 border-gray-800 bg-gray-900 rounded-lg place-self-center">
+                <Tooltip content={style.tooltip}>
+                  <Image
+                    className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] place-self-center"
+                    src={style.src}
+                    alt={style.alt}
+                    width={580}
+                    height={77}
+                    priority
+                  />
+                </Tooltip>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+
+      <div className="float-left">
+        <h2 className="float-right"></h2>
+      </div>
+    </main>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -45,6 +45,10 @@ export default function Home() {
     "https://upload.wikimedia.org/wikipedia/commons/2/2e/Manengouba_2.jpg",
     "https://upload.wikimedia.org/wikipedia/commons/c/c9/Mont-Cameroun.jpg",
     "https://media.routard.com/image/67/8/photo.1455678.jpg",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVpnaJAN3VSxAib_Rm4O7Joq7rESfjGH75Wlpa5NuVmWRB8TM5mmTxuSffv0SMI3yYseg&usqp=CAU",
+    "https://www.jeuneafrique.com/cdn-cgi/image/q=100,f=auto,metadata=none,width=1256,height=628/https://www.jeuneafrique.com/medias/2021/03/09/jad20210309-objectif-cameroun-tissus-ndop-1.jpg",
+    "https://dzaleu.net/wp-content/uploads/2019/12/desiree-babassagana-miss-nord-cameroun-en-kaba-robe-pagne-925x540-925x540.jpg",
+    "https://www.camer237.com/les-top-tenues-traditionelles-au-cameroun/",
     "https://fr.wikipedia.org/wiki/Libianca_Fonji",
     "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSSrbQtJXiaDrbUHdb-8_PCQitK-5KHSXL-Aw&usqp=CAU",
     "https://www.youtube.com/watch?v=MniqxLXANS4",
@@ -53,12 +57,17 @@ export default function Home() {
     "https://www.youtube.com/watch?v=s8v9Utq4nhg",
   ];
 
+  const citations = [""];
+
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+        <p
+          className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300
+                      bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30"
+        >
           L&apos;Afrique Francophone
         </p>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
@@ -109,6 +118,15 @@ export default function Home() {
                 <br></br>
               </>
             ))}
+          </div>
+        </div>
+        <div className="mb-12 mt-12 text-center text-2xl ">
+          <p>Citations:</p>
+          <div className="mb-12 mt-12 text-center text-sm ">
+            <a href="https://fr.wikipedia.org/wiki/Cameroun">
+              “Cameroun.” Wikipedia, The Free Encyclopedia, Wikimedia
+              Foundation, 5 mai 2023, https://fr.wikipedia.org/wiki/Cameroun.
+            </a>
           </div>
         </div>
       </div>
